35519f9f61fa34f75607894e7936e549840696d8
edilson gomes de lima <edskateicevida@gmail.com>
2023-07-16 01:14:43 -0300

    variavel

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("edy")
+console.log('bem vindo ao curso')
+
+
+
+
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+
+// let nome = 'edy';
+// console.log(nome);
+
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 30;
+// console.log(valorIngressoAdulto);
+
+const valorIngressoAdulto = 20;
+console.log(valorIngressoAdulto);
+
+let nome = 'edy';// string
+let idade = 25;//number bolean
+let estaAprovado = true;// underfined
+let sobrenome; //underfined
+let corSelecionado = null ; // redinir um valor
+
